c6a3158843b989dc4dd232bca582f56f80dadd13
ricardo <fondungallah@gmail.com>
2021-04-27 19:43:24 +0200

    added scroll

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -24,8 +24,8 @@ function onYouTubeIframeAPIReady() {
     }
 
     $(window).scroll(function () {
-        var top_of_element = $("#demo-1").offset().top;
-        var bottom_of_element = $("#demo-1").offset().top + $("#demo-1").outerHeight();
+        var top_of_element = $(".second-section").offset().top - 100;
+        var bottom_of_element = $(".second-section").offset().top + $(".second-section").outerHeight();
         var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
         var top_of_screen = $(window).scrollTop();
 
@@ -35,6 +35,7 @@ function onYouTubeIframeAPIReady() {
                 function () {
                     player.mute();
                     player.playVideo();
+                    console.log('playing now')
                 }, 1000)
 
         } else {
@@ -45,83 +46,9 @@ function onYouTubeIframeAPIReady() {
     function stopVideo() {
         player.playVideo();
     }
-    /* setTimeout(
-        function () {
-
-
-            $('.youtube-video').inViewAutoplay({
-                loop: 1,
-                autoplay: 0,
-                autohide: 1,
-                modestbranding: 1,
-                rel: 0,
-                quality: 'hd720',
-                controls: 0,
-                showinfo: 0
-
-            });
-
-            $('.youtube-video').inViewAutoplay({
-                loop: 1,
-                autoplay: 0,
-                autohide: 1,
-                modestbranding: 1,
-                rel: 0,
-                quality: 'hd720',
-                controls: 0,
-                showinfo: 0
-
-            });
-
-
-        }, 5000
-
-    ) */
-
 
 
 }
-
-
-/*$(window).scroll(function () {
-    var top_of_element = $("#demo-1").offset().top;
-    var bottom_of_element = $("#demo-1").offset().top + $("#demo-1").outerHeight();
-    var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
-    var top_of_screen = $(window).scrollTop();
-
-    if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
-        // the element is visible, do something
-        player.playVideo();
-    } else {
-        // the element is not visible, do something else
-        player.stopVideo();
-    }
-});*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 let buttons = document.querySelector(".mobile-slider__menu")
@@ -187,24 +114,26 @@ let timDisplayed__heading__tim = document.querySelector(".show-tim-btn__Tim");
 let timDisplayed__heading__Simon = document.querySelector(".show-tim-btn__Simon");
 
 
+if (toggleSimonBtn) {
+
+    toggleSimonBtn.addEventListener("click", function () {
+
+        timDisplayed__heading__Simon.classList.add("border-on-button")
+        timDisplayed__heading__tim.classList.remove("border-on-button")
+        timDisplayed.style.display = "none";
+        displaySimon.classList.remove("toggle-class");
+        displaySimon.style.display = "block"
+
+    })
+}
 
 
-toggleSimonBtn.addEventListener("click", function () {
+if (showTimbtn) {
+    showTimbtn.addEventListener("click", function () {
+        timDisplayed__heading__Simon.classList.remove("border-on-button")
+        timDisplayed__heading__tim.classList.add("border-on-button")
+        timDisplayed.style.display = "block";
+        displaySimon.style.display = "none";
 
-    timDisplayed__heading__Simon.classList.add("border-on-button")
-    timDisplayed__heading__tim.classList.remove("border-on-button")
-    timDisplayed.style.display = "none";
-    displaySimon.classList.remove("toggle-class");
-    displaySimon.style.display = "block"
-
-})
-
-
-
-showTimbtn.addEventListener("click", function () {
-    timDisplayed__heading__Simon.classList.remove("border-on-button")
-    timDisplayed__heading__tim.classList.add("border-on-button")
-    timDisplayed.style.display = "block";
-    displaySimon.style.display = "none";
-
-})
+    })
+}
